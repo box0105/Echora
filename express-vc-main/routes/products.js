c80a89e8449a1bf8ab778749fc3511491c13ac6e
Evelyn-guan <nancy19222717@gmail.com>
2025-02-08 14:48:23 +0800

    modify products api

--- express-vc-main/routes/products.js
+++ express-vc-main/routes/products.js
@@ -6,11 +6,11 @@ const router = express.Router()
 // GET /api/products
 router.get('/', async (req, res) => {
   try {
-    const [rows] =await db.query("SELECT * FROM product")
+    const [rows] = await db.query('SELECT * FROM product')
     res.status(200).json({
-        status: "success",
-        data: rows,
-        message: "取得資料成功"
+      status: 'success',
+      data: rows,
+      message: '取得資料成功',
     })
   } catch (err) {
     console.log(err)
@@ -19,35 +19,52 @@ router.get('/', async (req, res) => {
       message: err.message ? err.message : '取得資料失敗',
     })
   }
-});
+})
+
+// GET /api/products/search?q=
+router.get('/search', async (req, res) => {
+  const { q } = req.query
+  console.log(req.query)
+  try {
+    if (!q) throw new Error('請提供查詢字串')
+
+    const sql =
+      'SELECT product.*, brand.name AS brand_name FROM product JOIN brand ON product.brand_id = brand.id WHERE product.name LIKE ? OR brand.name LIKE ?'
+    const searchQuery = `%${q}%`
+    const [rows] = await db.query(sql, [searchQuery, searchQuery])
+
+    res.status(200).json({
+      status: 'success',
+      data: rows,
+      message: `搜尋成功, 條件: ${q}`,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: 'error',
+      message: err.message ? err.message : '搜尋失敗',
+    })
+  }
+})
 
 // GET /api/products/:pid
 router.get('/:pid', async (req, res) => {
-    const {pid} = req.params;
-    try {
-        const [rows] =await db.query(`SELECT * FROM product WHERE id = ${pid}`);
-        const row = rows[0];
-        res.status(200).json({
-            status: "success",
-            data: row,
-            message: "取得資料成功"
-        })
-      } catch (err) {
-        console.log(err)
-        res.status(400).json({
-          status: 'error',
-          message: err.message ? err.message : '取得資料失敗',
-        })
-      }
-})
-
-// GET /api/products/search
-// ask: error 連不到
-router.get('/search', async (req, res) => {
-    res.json({
-        status: "success",
-        message: "連線成功"
-    });
+  const { pid } = req.params
+  try {
+    const [rows] = await db.query(`SELECT * FROM product WHERE id = ${pid}`)
+    const row = rows[0]
+    res.status(200).json({
+      status: 'success',
+      data: row,
+      message: '取得資料成功',
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: 'error',
+      message: err.message ? err.message : '取得資料失敗',
+    })
+  }
 })
 
 // *ask: "比較功能"怎麼設路由? 從productlist的網址上拿pid參數?
