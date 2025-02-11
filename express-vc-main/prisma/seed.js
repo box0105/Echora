b92356052695bba941c387a3665c8480d55e9d6f
Mii <gueder85@gmail.com>
2025-02-11 11:30:27 +0800

    seed remove foreignKey

--- express-vc-main/prisma/seed.js
+++ express-vc-main/prisma/seed.js
@@ -12,11 +12,11 @@ import { isDev } from '../lib/utils.js'
 
 // 定義表關聯在這裡，目的是按正確順序的匯入種子資料，否則會出現外鍵約束錯誤或無法匯入的問題
 // foreignKey is in the second table
-const oneToOne = ['User:Profile']
-// foreignKey is in the second table
-const oneToMany = ['Category:Product', 'Brand:Product']
-// foreignKey is in the third table
-const manyToMany = ['User:Product:Favorite']
+// const oneToOne = ['User:Profile']
+// // foreignKey is in the second table
+// const oneToMany = ['Category:Product', 'Brand:Product']
+// // foreignKey is in the third table
+// const manyToMany = ['User:Product:Favorite']
 
 // seed檔案種類(副檔名)seed files extension (csv| json)
 const fileExtension = 'json'
