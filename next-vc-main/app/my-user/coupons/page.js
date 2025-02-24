3ae0c598d8ef52131f4b14183c886f9ec446f576
wabi172 <kknow321@gmail.com>
2025-02-24 10:49:04 +0800

    update

--- next-vc-main/app/my-user/coupons/page.js
+++ next-vc-main/app/my-user/coupons/page.js
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import '../_styles/coupons.scss'
-import './style.scss'
+// import './style.scss'
 import MemberLayout from '../layouts/memberLayout'
 import React, { useState, useEffect } from 'react'
 
@@ -17,8 +17,8 @@ export default async function OrderPage() {
         const res = await fetch(url)
         if (!res.ok) throw new Error('狀態錯誤')
         const data = await res.json()
-        console.log(data.data)
-        setCoupon(data.data)
+        console.log(data.userCheckCoupons)
+        setCoupon(data.userCheckCoupons)
       } catch (err) {
         console.log('發生錯誤', err)
       }
@@ -47,21 +47,6 @@ export default async function OrderPage() {
       <div className="body k-body">
         <div className="section-title h4 ">我的優惠券</div>
         <section className="section">
-          {/* <div className="gary-line div">
-            <Image
-              className="image"
-              src="/images/coupon/Vector.svg"
-              alt="Vector"
-              width={48}
-              height={48}
-            />
-            <ul>
-              <li>類型:</li>
-              <li>日期:</li>
-              <li>折扣:</li>
-            </ul>
-          </div>
-          <div className="gary-line div">123</div> */}
 
           {coupon.map((item) => (
             <div className="gary-line div">
@@ -83,55 +68,54 @@ export default async function OrderPage() {
                   </div>
                   <br />
 
-                  <button
+                  {/* <button
                     className="btn btn-dark"
                     onClick={() => {
                       notifyAndGet(item.name)
                     }}
                   >
                     領取
-                  </button>
+                  </button> */}
                 </div>
               </li>
             </div>
           ))}
 
-          {/* {coupon.map((v) =>
-            v.isDelete == true ? (
-              <div className="gary-line div">
-                <Image
-                  className="image"
-                  src="/images/coupon/Vector.svg"
-                  alt="Vector"
-                  width={48}
-                  height={48}
-                />
-                <li key={v.id} id={v.id}>
-                  <div className="image-col">
-                    <div className="text">
-                      <strong>{v.name}</strong>
-                      <br />
-                      使用日期: {time(v.startTime)}~{time(v.endTime)}
-                      <br />
-                      折扣: ${v.discount}
-                    </div>
+        <div className="section-title h4 mb-0">已使用/已失效</div>
+        <div className='clear'>全部清除</div>
+          {coupon.map((item) => item.isDeleted == true ?
+            <div className="gary-line div garybg">
+              <Image
+                className="image "
+                src="/images/coupon/Vector-b.svg"
+                alt="Vector"
+                width={48}
+                height={48}
+              />
+              <li key={item.id} id={item.id}>
+                <div className="image-col">
+                  <div className="text">
+                    <strong>{item.name}</strong>
                     <br />
-
-                    <button
-                      className="btn btn-dark"
-                      onClick={() => {
-                        notifyAndGet(v.name)
-                      }}
-                    >
-                      領取
-                    </button>
+                    使用日期: {time(item.startTime)}~{time(item.endTime)}
+                    <br />
+                    折扣: ${item.discount}
                   </div>
-                </li>
-              </div>
-            ) : (
-              ''
-            )
-          )} */}
+                  <br />
+
+                  <button
+                    className="btn btn-light"
+                    onClick={() => {
+                      notifyAndGet(item.name)
+                    }}
+                  >
+                    刪除
+                  </button>
+                </div>
+              </li>
+            </div>
+            : '' )}
+
         </section>
       </div>
     </MemberLayout>
