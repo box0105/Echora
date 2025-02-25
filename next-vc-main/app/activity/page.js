7864c70c5974476e2baa6ac5e7b286741f1094c6
box <timliu105@gmail.com>
2025-02-25 14:41:53 +0800

    活動前端9 - useActivity

--- next-vc-main/app/activity/page.js
+++ next-vc-main/app/activity/page.js
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
-import { useFetch } from '@/hooks/use-fetch'
+import React, { useState } from 'react'
+import { useActivity } from '@/hooks/use-activity'
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '../_styles/bootstrap.scss'
@@ -14,74 +14,29 @@ import HeroSection from './_components/HeroSection'
 import Title from './_components/Title'
 
 export default function ActivityPage() {
-  // Fetch Data
-  const { data: acts, isLoading } = useFetch(
-    `http://localhost:3005/api/activities/`
-  )
+  // Fetch Data, sort & filter & search
+  const {
+    displayActs,
+    isLoading,
+    sortByKey,
+    getRandomPhotos,
+  } = useActivity(`http://localhost:3005/api/activities/`);
 
-  const [displayActs, setDisplayActs] = useState([])
-  useEffect(() => {
-    if (acts) {
-      setDisplayActs(acts)
-    }
-  }, [acts])
 
   // Filter Switch
   const [isFilterOpen, setIsFilterOpen] = useState(false)
 
   if (isLoading) return <h3>網頁載入中，請稍後...</h3>
 
-  const sortPrice = (order) => {
-    const sorted = [...acts].sort((a, b) => {
-      const x = a.type[0].price
-      const y = b.type[0].price
-      return order === 'desc' ? y - x : x - y
-    })
-    setDisplayActs(sorted)
-  }
-
-  const sortId = (order) => {
-    const sorted = [...acts].sort((a, b) => {
-      const x = a.id
-      const y = b.id
-      return order === 'desc' ? y - x : x - y
-    })
-    setDisplayActs(sorted)
-  }
-
-  const sortDate = (order) => {
-    const sorted = [...acts].sort((a, b) => {
-      const x = new Date(a.date_start).getTime()
-      const y = new Date(b.date_start).getTime()
-      return order === 'desc' ? y - x : x - y
-    })
-    setDisplayActs(sorted)
-  }
-
-  // 隨機選取封面照
-  // const getRandomPhotos = (acts, num) => {
-  //   const randomIndices = new Set()
-
-  //   // C(全部, num)
-  //   while (randomIndices.size < num) {
-  //     const randomIndex = Math.floor(Math.random() * acts.length)
-  //     randomIndices.add(randomIndex)
-  //   }
-
-  //   const photos = Array.from(randomIndices).map(
-  //     (index) => acts[index].media.split(',')[0]
-  //   )
-
-  //   return photos
-  // }
-  // const randomPhotos = getRandomPhotos(acts, 3)
+   // Get random photos
+   const randomPhotos = getRandomPhotos(3);
 
   return (
     <div className="b-container px-0">
       <HeroSection
         title="2025 全台音樂祭"
         subTitle="一同締造屬於你的冒險拾光"
-        // images={randomPhotos}
+        images={randomPhotos}
       />
 
       <div className="b-container">
@@ -103,7 +58,7 @@ export default function ActivityPage() {
             <button
               className="b-order b-btn-unstyled d-flex align-items-baseline"
               type="button"
-              onClick={() => sortDate('desc')}
+              onClick={() => sortByKey('price', 'desc')}
             >
               <i className="fa-solid fa-arrow-up-wide-short" />
               <h4>排序</h4>
