63de6d20e4ff6c28bc008b7cfe69e781da91e2b6
wabi172 <kknow321@gmail.com>
2025-02-27 15:54:52 +0800

    update

--- next-vc-main/app/my-user/coupons/page.js
+++ next-vc-main/app/my-user/coupons/page.js
@@ -2,46 +2,54 @@
 
 import Image from 'next/image'
 import '../_styles/coupons.scss'
-// import './style.scss'
 import MemberLayout from '../layouts/memberLayout'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { useMyCoupon } from '@/hooks/use-coupon'
 
 
-export default async function OrderPage() {
+
+export default function UserCouponPage() {
   const [coupon, setCoupon] = useState([])
-  const userId = localStorage.getItem('userId')
+  const { time, clear } = useMyCoupon()
+  const [user, setUser] = useState(null)
+  const [btn, setBtn] = useState(false)
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = `http://localhost:3005/api/coupon/${userId}`
-        const res = await fetch(url)
-        if (!res.ok) throw new Error('狀態錯誤')
-        const data = await res.json()
-        console.log(data.userCheckCoupons)
-        setCoupon(data.userCheckCoupons)
-      } catch (err) {
-        console.log('發生錯誤', err)
-      }
-    }
-    fetchData()
+    const user = localStorage.getItem('userId')
+    setUser(user)
   }, [])
 
-  // 轉換時間格式
-  const time = (time) => {
-    if (!time) {
-      console.log('沒有時間')
-    } else console.log('輸入時間')
+  //useCallback(fn,deps)記憶一個函數 當(fn,deps)的deps(依賴項陣列)值變化才會重新建立函數 節省效能
+  const fetchData = useCallback(async () => {
+    if (!user) return; // 確保 user 存在
+    try {
+      const url = `http://localhost:3005/api/coupon/${user}`
+      const res = await fetch(url)
+      if (!res.ok) throw new Error('狀態錯誤')
+      const data = await res.json()
+      console.log(data.userCheckCoupons)
+      setCoupon(data.userCheckCoupons)
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, [user]) //空陣列 表示fetchData沒有依賴任何外部變數
 
-    const isoDateString = time
-    const date = new Date(isoDateString)
+  useEffect(() => {
+    if (user) {
+      fetchData()
+    }
+  }, [fetchData, btn, user])
 
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
-    const readableDate = date.toLocaleDateString('zh-TW', options) // 繁體中文
+  const handleClearClick = async () => {
+    if (!user) return; // 確保 user 存在
+    // 呼叫 clear 函數 
+    await clear(user);
+    // 設定 btn 狀態變數為 true，觸發 useEffect Hook 重新執行
+    setBtn(prev => !prev);
+  };
 
-    // console.log(readableDate); 輸出：2024/7/27 （或 2024年7月27日，取決於地區設定）
-    return readableDate
-  }
+
 
   return (
     <MemberLayout>
@@ -49,8 +57,8 @@ export default async function OrderPage() {
         <div className="section-title h4 ">我的優惠券</div>
         <section className="section">
 
-        {coupon.map((item) =>  item.isDeleted == false ?
-             <div className="gary-line div">
+          {coupon.map((item) => item.isDeleted == false ?
+            <div className="gary-line div" key={item.id}>
               <Image
                 className="image"
                 src="/images/coupon/Vector.svg"
@@ -82,10 +90,15 @@ export default async function OrderPage() {
             </div> : ''
           )}
 
-          <div className="section-title h4 mb-0">已使用/已失效</div>
-          <div className='clear'>全部清除</div>
+          <div className="section-title h4 mb-0">
+            已使用/已失效
+            <div className='clear'><button className='btn btn-dark' onClick={async () => {
+              handleClearClick()
+            }}>全部清除</button></div>
+
+          </div>
           {coupon.map((item) => item.isDeleted == true ?
-            <div className="gary-line div garybg">
+            <div className="gary-line div garybg" key={item.id}>
               <Image
                 className="image "
                 src="/images/coupon/Vector-b.svg"
