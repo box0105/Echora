de5313d75baa7ccb8f0ebd6e53ad68c700de2454
box <timliu105@gmail.com>
2025-09-09 23:22:34 +0800

    spinner-3

--- next-vc-main/app/activity/_components/Spinner.js
+++ next-vc-main/app/activity/_components/Spinner.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "../_styles/spinner.scss";
 
-export default function Spinner({ message = "伺服器啟動中，請稍後" }) {
+export default function Spinner({ message = "資料載入中..." }) {
   return (
     <div className="spinner-wrapper">
-      <div className="spinner"></div>
+      <div className="wave-spinner">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <p>{message}</p>
     </div>
   );
 }
-
